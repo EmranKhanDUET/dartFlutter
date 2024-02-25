28ca2e272013d34990430a64488273b9bb13091a
EMRAN KHAN <emrankhanent@gmail.com>
2024-02-25 16:34:15 +0600

    This is an update

--- OOP/object.dart
+++ OOP/object.dart
@@ -1,3 +1,20 @@
-void main(){
+class myClass {
+  var myName = 'Emran';
+  List Alpha = [1, 2, 3, 4];
 
+  void addTwonumbers(int x, int y) {
+    print(x + y);
+  }
+
+  void addThreenumbers(int x ,int y ,int z){
+    print(x+y+z);
+  }
+}
+
+void main() {
+  var obj = new myClass();
+  obj.addTwonumbers(10, 2);
+  obj.addThreenumbers(10, 2,5);
+  print(obj.myName);
+  print(obj.Alpha);
 }
