948015ea2a3c15d9ea96872e5c6384dae67fb5a6
EMRAN KHAN <emrankhanent@gmail.com>
2024-02-27 19:01:56 +0600

    This is an update

--- OOP/Encapsulation/6 Encapsulation.dart
+++ OOP/Encapsulation/6 Encapsulation.dart
@@ -1,12 +1,22 @@
 // same file e main function and class thakle dart e
-// encapsulation hoy nah, 2 ta k alada alada file e rekhe
+// encapsulation(Identifier er age underscore) hoy nah, 2 ta k alada alada file e rekhe
 // import korte hoy
-
 
 import '6 class.dart';
 
-void main(){
-var randomPerson=new Person('Emran', 'Khan', 'Male', 22);
-print(randomPerson.firstName);
-print(randomPerson.getFullName());
+void main() {
+  var randomPerson = new Person('Emran', 'Khan', 'Male', 22);
+
+// Can't access directly because of encapsulation'
+// print(randomPerson._firstName);
+  print(randomPerson.getFullName());
+  print(randomPerson.getGender());
+  randomPerson.changeGender('Female');
+  print(randomPerson.getGender());
+
+//using get & set keyword
+  print(randomPerson.getAge);
+
+  randomPerson.setAge = 25;
+  print(randomPerson.getAge);
 }
---
+++ OOP/Inheritance/Class.dart
@@ -0,0 +1,10 @@
+// parent/super/base class
+class Man{
+
+}
+
+
+// child class
+class Student{
+
+}
