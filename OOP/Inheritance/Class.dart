4a1dc761bb79b3f518ff3138c9f9d91a1af30f02
EMRAN KHAN <emrankhanent@gmail.com>
2024-02-27 19:26:36 +0600

    This is an update

--- OOP/Inheritance/Class.dart
+++ OOP/Inheritance/Class.dart
@@ -1,10 +1,41 @@
 // parent/super/base class
-class Man{
+class Man {
+  int hands;
+  int eyes;
+  int legs;
 
+  Man(this.hands, this.eyes, this.legs);
+
+  void moving() {
+    print('Moving');
+  }
+
+  void eating() {
+    print('Eating');
+  }
+
+  void talking() {
+    print('Talking');
+  }
 }
 
-
 // child class
-class Student{
+class Student extends Man {
+  String institute;
+  String section;
 
+  // Student(int hands, int eyes, int legs, this.institute, this.section)
+  //     : super(hands, eyes, legs);
+  Student(super.hands, super.eyes, super.legs, this.institute, this.section);
+
+  @override
+  void talking() {
+    print('Talking politely');
+  }
+
+  @override
+  void eating() {
+    super.eating();
+    print('Student Eating');
+  }
 }
