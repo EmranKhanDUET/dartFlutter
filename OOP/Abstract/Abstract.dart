34e47e611930179b46051af477a259e06f7cae93
EMRAN KHAN <emrankhanent@gmail.com>
2024-02-29 10:41:24 +0600

    This is an update

---
+++ OOP/Abstract/Abstract.dart
@@ -0,0 +1,10 @@
+import 'class.dart';
+
+void main() {
+  // Student student1=new Student();
+  CSEStudent student2 = new CSEStudent();
+  student2.reading();
+
+  PharmacyStudent student3 = new PharmacyStudent();
+  student3.reading();
+}
---
+++ OOP/Abstract/class.dart
@@ -0,0 +1,48 @@
+//
+// jokhon kono class k skeleton hisabe use kora hoy
+// not as a main class , tokhon take abstract class bole
+// // abstract class er object create kora jay nah
+// abstract e jodi kono unimplemented method thake, jemon  void reading(), tahole
+// oi method k extended class e obossoi define korte hobe,
+//
+// jodi implemented method thake jemon void eating(), tahole extended Class
+// k r force korbe nah
+
+abstract class Student {
+  void reading();
+
+  void eating() {
+    print('Eating food');
+  }
+}
+
+class CSEStudent extends Student {
+  @override
+  void reading() {
+    print('Reading some code');
+  }
+}
+
+class PharmacyStudent extends Student {
+  @override
+  void reading() {
+    print('Reading Chemistry');
+  }
+}
+
+//
+// jodi implememnts keyword use kora hoy, insted of extends
+// , tahole abstract class er implemented and unimplemented,
+// sokol method kei abar override korte hobe
+
+class EEEStudent implements Student {
+  @override
+  void eating() {
+    print("Eating ion");
+  }
+
+  @override
+  void reading() {
+    print('Reading Circuit Book');
+  }
+}
