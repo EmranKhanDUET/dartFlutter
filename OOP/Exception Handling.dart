bd2cf94f8377e826947a5faca109e2ceecf4c188
EMRAN KHAN <emrankhanent@gmail.com>
2024-03-09 17:16:46 +0600

    This is an update

--- OOP/Exception Handling.dart
+++ OOP/Exception Handling.dart
@@ -2,22 +2,30 @@
 
 // catch => used to get exception type without being crashed in program
 // on => used to perform specific code on specific types of exception
+// finally => used at the end of the code which is always being executed
+//throw=> used to make Custom exception
 
 void main() {
   try {
     calculation();
-  }on FormatException{
+  } on FormatException {
+    //if on-block performed then catch-block will not be executed
     print("FormatException Happened");
-  }
-  catch (exception) {
+  } catch (exception) {
     //autometically catches exception type
     print(exception);
-  }finally{
+  } finally {
+    //finally-block always being executed
     print("Exception handled successfully");
   }
 }
 
 void calculation() {
+
   // int result = 10 ~/ 0; //IntegerDivisionByZeroException
   int x = int.parse('10a'); //FormatException
+  throw "Cant't Handle"; //throw=> used to make Custom exception
 }
+
+
+//
