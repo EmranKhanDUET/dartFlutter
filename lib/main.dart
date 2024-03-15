f951ab4098881990d650194f6dbd62f08686e1af
emran <emrankhanent@gmail.com>
2024-03-15 23:50:59 +0600

    this is an update

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,16 @@ void main() {
     {'Name': 'Banana', 'Color': 'Yellow', 'Price': 1.0},
     {'Name': 'Grapes', 'Color': 'Purple', 'Price': 3.0},
   ];
-  int discountRate=10;
+  int discountRate=10;  //Cosidering 10% as discount rate
+
+  // Displaying price chart before applying discount
   print("Original Fruit Details before Discount:");
   displayFruitDetails(inpList: fruits);
+
+  // Applying discount
   List discountedFruits = applyPriceDiscount(inpList: fruits, discount: discountRate);
+
+  // Displaying price chart after applying discount
   print("Fruit Details After Applying ${discountRate}% Discount:");
   displayFruitDetails(inpList: discountedFruits);
 }
