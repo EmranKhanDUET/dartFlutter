6ed6ea4b94a93fc53126169c7f342268027fbdda
emran <emrankhanent@gmail.com>
2024-03-15 22:38:58 +0600

    this is an update

--- OOP/Inheritance/Inheritance.dart
+++
@@ -1,9 +0,0 @@
-import 'Class.dart';
-void main(){
-  Student studentOne=new Student(2, 2, 2, 'DUET', 'A');
-  studentOne.moving();
-  print(studentOne.legs);
-  studentOne.talking();
-  studentOne.eating();
-
-}
--- basicDart/HelloWorld.dart
+++
@@ -1,4 +0,0 @@
-void main(){
-  print("Hello World");
-  // int x=10;
-}
